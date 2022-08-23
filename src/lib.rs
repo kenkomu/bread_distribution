f4cb17e6ad21bccc1f9a319259c4a60d0e17e6b8
kenkomu <komukenneth216@gmail.com>
2022-08-23 11:11:21 +0300

    bread distribution project

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
-use near_sdk::collections::{UnorderedMap};
+use near_sdk::collections::UnorderedMap;
 
-use near_sdk::serde::{Deserialize, Serialize};
+//use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{env, near_bindgen};
 // bread whole
 // different types
@@ -12,6 +12,13 @@ struct Bread {
     name: String,
     date_created: String,
     code: u16,
+}
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize, Debug)]
+struct Customer {
+    name: String,
+    date_created: String,
+    code: u8,
 }
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
@@ -41,23 +48,16 @@ impl Default for Shop {
 pub struct BreadDistributor {
     shops: Vec<Shop>,
     breads: Vec<Bread>,
+    customer: Vec<Customer>,
 }
-
-// impl Default for BreadDistributor {
-//     fn default() -> Self {
-//         BreadDistributor {
-//                 shops: Vector::new(b"r".to_vec()),
-//                 breads: Vector::new(b"r".to_vec()),
-//             }
-//     }
-// }
 
 #[near_bindgen]
 impl BreadDistributor {
     #[init]
     pub fn new() -> Self {
-        let shop: Vec<Shop> = vec![];// Vector::new(b"r".to_vec());
-        let mut bread: Vec<Bread> =vec![]; // Vector::new(b"r".to_vec());
+        let shop: Vec<Shop> = vec![]; // Vector::new(b"r".to_vec());
+        let mut bread: Vec<Bread> = vec![]; // Vector::new(b"r".to_vec());
+        let  customer: Vec<Customer> = vec![]; // Vector::new(b"r".to_vec());
 
         let kenblest = Bread {
             name: "kenblest".to_string(),
@@ -69,6 +69,7 @@ impl BreadDistributor {
         BreadDistributor {
             shops: shop,
             breads: bread,
+            customer: customer,
         }
     }
 
@@ -81,13 +82,34 @@ impl BreadDistributor {
         self.breads.push(br);
     }
 
-    pub fn supply_bread(&mut self, shop_id: u16, name: String, amount: u8) {
-        let mut shop_exits: bool = false;
-        self.shops.iter_mut().for_each(|shop| {
-            if shop.id == shop_id {
-                shop_exits = true;
+    pub fn add_shop(
+        &mut self,
+        name: String,
+        location: String,
+        inventory_size: u8,
+        minumum_invetory_size: u8,
+    ) -> u16 {
+        let shop_id = self.shops.len() + 1;
 
-                shop.inventory_size += amount;
+        let br = Shop {
+            id: shop_id as u16,
+            name: name,
+            location: location,
+            inventory_size: inventory_size,
+            minumum_invetory_size: minumum_invetory_size,
+            bread_amount: UnorderedMap::new(b"r".to_vec()),
+        };
+        self.shops.push(br);
+        shop_id as u16
+    }
+
+    pub fn supply_bread(&mut self, shop_id: u16, name: String, amount: u8) {
+        // let mut shop_exits: bool = false;
+        // self.shops.iter_mut().for_each(|shop| {
+
+        for shop in self.shops.iter_mut() {
+            if shop.id == shop_id {
+                shop.inventory_size = shop.inventory_size + amount;
 
                 let bread_current = shop.bread_amount.get(&name);
 
@@ -100,15 +122,23 @@ impl BreadDistributor {
                         shop.bread_amount.insert(&name, &amount);
                     }
                 }
+                break;
             }
-        });
+        }
     }
-    
-    pub fn shop_sell(&mut self, id_shop: u16, name: String, quantity: u8) {
-        self.shops.iter_mut().for_each(|shop: &mut Shop| {
+
+    pub fn shop_sell(
+        &mut self,
+        id_shop: u16,
+        bread_name: String,
+        quantity: u8,
+        customer_name: String,
+    ) {
+        // self.shops.iter_mut().for_each(|shop: &mut Shop| {
         // for (index, shop) in &mut self.shops.iter().enumerate() {
+        for shop in self.shops.iter_mut() {
             if shop.id == id_shop {
-                let bread_current = shop.bread_amount.get(&name);
+                let bread_current = shop.bread_amount.get(&bread_name);
 
                 match bread_current {
                     Some(n) => {
@@ -116,21 +146,28 @@ impl BreadDistributor {
                         // let tmp_shop: Shop = shop;
 
                         let total = n - quantity;
-                        shop.bread_amount.insert(&name, &total); //= n+amount;
+                        shop.bread_amount.insert(&bread_name, &total); //= n+amount;
 
+                        let cs = Customer {
+                            name: customer_name.clone(),
+                            code: n,
+                            date_created: "".to_owned(),
+                        };
+                        self.customer.push(cs);
                         // let tmp_ind: u64=   index.try_into().expect("msg");
                         // self.shops.replace( tmp_ind, &tmp_shop);
                     }
                     None => {
-                        let my_str = format!("There is no bread of type {}", name);
-                        env::log(my_str.as_bytes());
+                        let my_str = format!("There is no bread of type {}", bread_name);
+                        
+                        let s_slice: &str = &my_str[..];
+                        env::log_str(s_slice);
                     }
                 }
             }
+        }
 
-        // }
-
-        });
+        // });
     }
 }
 
@@ -146,13 +183,57 @@ mod tests {
     use super::*;
     // TESTS HERE
     #[test]
-    fn add_course() {
+    fn add_bread() {
         let mut bread_dist = BreadDistributor::new();
 
         bread_dist.add_bread("broadways".to_owned(), 111, "date".to_owned());
         bread_dist.add_bread("supa loaf".to_owned(), 113, "date".to_owned());
         assert_eq!(bread_dist.breads.len(), 3);
-       
     }
 
+    // we were to write tests
+
+    #[test]
+    fn supply_bread() {
+        let mut bread_dist = BreadDistributor::new();
+        bread_dist.add_bread("broadways".to_owned(), 111, "date".to_owned());
+
+        let shop_id = bread_dist.add_shop("ken shop".to_string(), "nairobi".to_string(), 0, 10);
+
+        assert_eq!(bread_dist.breads.len(), 2);
+        assert_eq!(bread_dist.shops.len(), 1);
+
+        //supply
+        bread_dist.supply_bread(shop_id, "broadways".to_owned(), 1);
+
+
+        let mut bread_amount = 0;
+        for elem in bread_dist.shops.iter() {
+            if elem.id == shop_id {
+                assert_eq!(elem.inventory_size, 1);
+
+                let res = elem.bread_amount.get(&"broadways".to_string());
+
+                match res {
+                    Some(g) => {
+                        bread_amount = g;
+                    }
+                    None => {
+                      
+                        panic!("error not found")
+                    }
+                }
+            }
+        }
+        assert_eq!(bread_amount, 1);
+
+    }
+
+    #[test]
+    fn shop_sell() {
+        let mut bread_dist = BreadDistributor::new();
+        let shop_id = bread_dist.add_shop("ken shop".to_string(), "nairobi".to_string(), 0, 10);
+        bread_dist.shop_sell(shop_id, "kenblest".to_owned(),22,"Kenn".to_owned() );
+        assert_eq!(bread_dist.shops.len(), 1);
+    }
 }
